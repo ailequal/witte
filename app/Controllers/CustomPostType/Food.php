1c300eab2123a272337516cbd5660202e8e249b3
ailequal <37016865+ailequal@users.noreply.github.com>
2022-03-12 10:41:34 +0100

    Fixing labels.

--- app/Controllers/CustomPostType/Food.php
+++ app/Controllers/CustomPostType/Food.php
@@ -15,6 +15,13 @@ class Food extends Hook
     use Singleton;
 
     /**
+     * The food slug.
+     *
+     * @var string
+     */
+    protected $slug = 'witte_food';
+
+    /**
      * Loads all the hooks related to this class.
      */
     public function hooks()
@@ -30,31 +37,31 @@ class Food extends Hook
         $labels = [
             'name'                  => _x('Foods', 'Post Type General Name', 'witte'),
             'singular_name'         => _x('Food', 'Post Type Singular Name', 'witte'),
-            'menu_name'             => __('Post Types', 'witte'),
-            'name_admin_bar'        => __('Post Type', 'witte'),
-            'archives'              => __('Item Archives', 'witte'),
-            'attributes'            => __('Item Attributes', 'witte'),
-            'parent_item_colon'     => __('Parent Item:', 'witte'),
-            'all_items'             => __('All Items', 'witte'),
-            'add_new_item'          => __('Add New Item', 'witte'),
+            'menu_name'             => __('Foods', 'witte'),
+            'name_admin_bar'        => __('Food', 'witte'),
+            'archives'              => __('Food Archives', 'witte'),
+            'attributes'            => __('Food Attributes', 'witte'),
+            'parent_item_colon'     => __('Parent Food:', 'witte'),
+            'all_items'             => __('All Foods', 'witte'),
+            'add_new_item'          => __('Add New Food', 'witte'),
             'add_new'               => __('Add New', 'witte'),
-            'new_item'              => __('New Item', 'witte'),
-            'edit_item'             => __('Edit Item', 'witte'),
-            'update_item'           => __('Update Item', 'witte'),
-            'view_item'             => __('View Item', 'witte'),
-            'view_items'            => __('View Items', 'witte'),
-            'search_items'          => __('Search Item', 'witte'),
+            'new_item'              => __('New Food', 'witte'),
+            'edit_item'             => __('Edit Food', 'witte'),
+            'update_item'           => __('Update Food', 'witte'),
+            'view_item'             => __('View Food', 'witte'),
+            'view_items'            => __('View Foods', 'witte'),
+            'search_items'          => __('Search Food', 'witte'),
             'not_found'             => __('Not found', 'witte'),
             'not_found_in_trash'    => __('Not found in Trash', 'witte'),
             'featured_image'        => __('Featured Image', 'witte'),
             'set_featured_image'    => __('Set featured image', 'witte'),
             'remove_featured_image' => __('Remove featured image', 'witte'),
             'use_featured_image'    => __('Use as featured image', 'witte'),
-            'insert_into_item'      => __('Insert into item', 'witte'),
-            'uploaded_to_this_item' => __('Uploaded to this item', 'witte'),
-            'items_list'            => __('Items list', 'witte'),
-            'items_list_navigation' => __('Items list navigation', 'witte'),
-            'filter_items_list'     => __('Filter items list', 'witte'),
+            'insert_into_item'      => __('Insert into food', 'witte'),
+            'uploaded_to_this_item' => __('Uploaded to this food', 'witte'),
+            'items_list'            => __('Foods list', 'witte'),
+            'items_list_navigation' => __('Foods list navigation', 'witte'),
+            'filter_items_list'     => __('Filter foods list', 'witte'),
         ];
 
         $args = [
@@ -77,7 +84,7 @@ class Food extends Hook
             'capability_type'     => 'page',
         ];
 
-        register_post_type('witte_food', $args);
+        register_post_type($this->slug, $args);
     }
 
 }

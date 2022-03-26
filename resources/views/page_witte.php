eab5f62b56686d796381e17b093118a0ab3133f3
ailequal <37016865+ailequal@users.noreply.github.com>
2022-03-26 16:02:06 +0100

    Added time.

--- resources/views/page_witte.php
+++ resources/views/page_witte.php
@@ -10,6 +10,8 @@
 
 get_header('witte');
 
+$time = wp_date('H:m - d-m-Y', time(), null);
+
 // TODO: We need to handle these labels with their translations??
 //  Or maybe just add universal label textarea inside the options.
 //$meals = [
@@ -27,6 +29,8 @@ $dinner = $day['dinner'];
 
         <h2>witte</h2>
 
+        <h3 class="time"><?php echo $time; ?></h3>
+
         <!--            --><?php //foreach ($day as $mealKey => $mealData): ?>
         <!--                --><?php //// Loop all the meals together. ?>
         <!--            --><?php //endforeach; ?>
@@ -36,11 +40,10 @@ $dinner = $day['dinner'];
             <?php foreach ($lunch as $mealKey => $mealData): ?>
                 <?php // Loop the lunch meal.
                 if (0 == $mealData['id'])
-                    continue;
-                ?>
+                    continue; ?>
                 <div class="meal">
-                    <h3><?php echo implode(' - ', $mealData['translations']) ?></h3>
-                    <?php echo $mealData['thumbnail'] ?>
+                    <h3><?php echo implode(' - ', $mealData['translations']); ?></h3>
+                    <?php echo $mealData['thumbnail']; ?>
                 </div>
             <?php endforeach; ?>
         </div>
@@ -50,11 +53,10 @@ $dinner = $day['dinner'];
             <?php foreach ($dinner as $mealKey => $mealData): ?>
                 <?php // Loop the dinner meal.
                 if (0 == $mealData['id'])
-                    continue;
-                ?>
+                    continue; ?>
                 <div class="meal">
-                    <h3><?php echo implode(' - ', $mealData['translations']) ?></h3>
-                    <?php echo $mealData['thumbnail'] ?>
+                    <h3><?php echo implode(' - ', $mealData['translations']); ?></h3>
+                    <?php echo $mealData['thumbnail']; ?>
                 </div>
             <?php endforeach; ?>
         </div>

64432c3001d0b34380dc0966e32215c4b9472ab4
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-16 09:55:39 +0200

    Added translations column.

--- app/App.php
+++ app/App.php
@@ -84,6 +84,7 @@ class App
         Controllers\OptionPage\WeekPlan\Data::getInstance()->injectDependency('week', Controllers\Week::getInstance());
         Controllers\OptionPage\WeekPlan\Data::getInstance()->injectDependency('courseData', Controllers\CustomPostType\Course\Data::getInstance());
 
+        Controllers\CustomPostType\Course\Column::getInstance()->injectDependency('courseData', Controllers\CustomPostType\Course\Data::getInstance());
         Controllers\CustomPostType\Course\MetaBox::getInstance()->injectDependency('optionData', Controllers\OptionPage\Option\Data::getInstance());
         Controllers\CustomPostType\Course\Data::getInstance()->injectDependency('optionData', Controllers\OptionPage\Option\Data::getInstance());
 
--- app/Controllers/CustomPostType/Course/Column.php
+++ app/Controllers/CustomPostType/Course/Column.php
@@ -3,16 +3,22 @@
 namespace Ailequal\Plugins\Witte\Controllers\CustomPostType\Course;
 
 use Ailequal\Plugins\Witte\Abstracts\Hook;
+use Ailequal\Plugins\Witte\Controllers\Week;
+use Ailequal\Plugins\Witte\Traits\DependencyInjection;
 use Ailequal\Plugins\Witte\Traits\Singleton;
 
 /**
  * The Column plugin class for the course.
  * Define the custom columns for the course.
+ *
+ * All the dependencies injected as magic methods:
+ * @property Data $courseData
  */
 class Column extends Hook
 {
 
     use Singleton;
+    use DependencyInjection;
 
     /**
      * Loads all the hooks related to this class.
@@ -21,6 +27,9 @@ class Column extends Hook
     {
         add_filter('manage_course_posts_columns', [$this, 'injectColumnHeaderThumbnail'], 10, 1);
         add_action('manage_course_posts_custom_column', [$this, 'injectColumnContentThumbnail'], 10, 2);
+
+        add_filter('manage_course_posts_columns', [$this, 'injectColumnHeaderTranslations'], 11, 1);
+        add_action('manage_course_posts_custom_column', [$this, 'injectColumnContentTranslations'], 11, 2);
     }
 
     /**
@@ -38,7 +47,7 @@ class Column extends Hook
         // Set the thumbnail column right after the title one.
         // @link https://stackoverflow.com/questions/3353745/how-to-insert-element-into-arrays-at-specific-position
         return array_slice($postColumns, 0, 2, true) +
-               ['thumbnail' => __('Thumbnails')] +
+               ['thumbnail' => __('Thumbnails', 'witte')] +
                array_slice($postColumns, 2, count($postColumns) - 1, true);
     }
 
@@ -58,6 +67,49 @@ class Column extends Hook
             $thumbnail = '—';
 
         echo $thumbnail;
+    }
+
+    /**
+     * Add the translations column header in the backend.
+     *
+     * @param  array  $postColumns
+     *
+     * @return array
+     */
+    public function injectColumnHeaderTranslations($postColumns)
+    {
+        if (true == array_key_exists('translations', $postColumns))
+            return $postColumns;
+
+        // Set the translations column right after the thumbnail one.
+        // @link https://stackoverflow.com/questions/3353745/how-to-insert-element-into-arrays-at-specific-position
+        return array_slice($postColumns, 0, 3, true) +
+               ['translations' => __('Translations', 'witte')] +
+               array_slice($postColumns, 3, count($postColumns) - 1, true);
+    }
+
+    /**
+     * Add the translations column content in the backend.
+     *
+     * @param  string  $columnName
+     * @param  int  $post_id
+     */
+    public function injectColumnContentTranslations($columnName, $post_id)
+    {
+        if ('translations' != $columnName)
+            return;
+
+        $translations = $this->courseData->getTranslations($post_id);
+        ?>
+        <ul style="display: flex; flex-direction: column; justify-content: space-between; align-items: start; width: 100%; min-height: 150px; margin: auto;">
+            <?php foreach ($translations as $key => $translation): ?>
+                <li>
+                    <span style="font-weight: bold;"><?php echo strtoupper($key) ?></span>
+                    <span><?php echo $translation ?></span>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <?php
     }
 
 }

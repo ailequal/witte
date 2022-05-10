415b782f633c66642669069902589946ce25854b
ailequal <37016865+ailequal@users.noreply.github.com>
2022-05-10 21:56:24 +0200

    Added Poppins font.

--- app/Controllers/Template.php
+++ app/Controllers/Template.php
@@ -29,8 +29,24 @@ class Template extends Hook
      */
     public function hooks()
     {
+        add_action('wp_head', [$this, 'injectFonts'], 10, 1);
         add_action('wp_enqueue_scripts', [$this, 'frontendEnqueue'], 10, 1);
 //        add_filter('show_admin_bar', '__return_false'); // TODO: Only for debugging.
+    }
+
+    /**
+     * Inject the fonts for the frontend.
+     */
+    public function injectFonts()
+    {
+        if (is_page_template('page_witte.php')) {
+            ?>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400&display=swap"
+                  rel="stylesheet">
+            <?php
+        }
     }
 
     /**

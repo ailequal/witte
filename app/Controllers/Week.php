496f2f5d040df5e47ee3b800e482bdf0f7657a31
ailequal <37016865+ailequal@users.noreply.github.com>
2022-06-24 14:06:43 +0200

    fixed the timezone offset

--- app/Controllers/Week.php
+++ app/Controllers/Week.php
@@ -82,11 +82,33 @@ class Week
      */
     public function getToday()
     {
-        // TODO: Test and decide if we should instead use the current_time() function from WordPress itself,
-        //  which might be more accurate, since it should reflect the current timezone settings in the backend.
-        $day = date("l", time()); // It should never return false, but still check it.
+        // We do not rely on wp_date() since it would automatically translate the day label into the current
+        // WordPress active language. That's why we manually add the offset. Both date() and time() always handle the
+        // unix timestamp (UTC format), no matter how the server is configured.
+        $day = date("l", time() + $this->getTimezoneOffset()); // It should never return false, but still check it.
 
         return (false == $day) ? 'monday' : strtolower($day);
+    }
+
+    /**
+     * Get timezone offset in seconds.
+     * This function is copied from WooCommerce 6.6.1.
+     *
+     * @return float
+     *
+     * @link https://github.com/woocommerce/woocommerce/blob/trunk/plugins/woocommerce/includes/wc-formatting-functions.php#L794
+     */
+    public function getTimezoneOffset()
+    {
+        $timezone = get_option('timezone_string');
+
+        if ($timezone) {
+            $timezone_object = new \DateTimeZone($timezone);
+
+            return $timezone_object->getOffset(new \DateTime('now'));
+        } else {
+            return floatval(get_option('gmt_offset', 0)) * HOUR_IN_SECONDS;
+        }
     }
 
 }
